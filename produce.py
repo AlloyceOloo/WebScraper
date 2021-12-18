1d2a1143e9603111a1dc4c4bfca7b049a9cbece1
Alloyce Oloo <45062907+AlloyceOloo@users.noreply.github.com>
2021-12-18 08:08:22 +0300

    Update produce.py

--- produce.py
+++ produce.py
@@ -16,23 +16,25 @@ def main():
     from pymongo import MongoClient
     
 
-
+    #initialize the variables to store property information
     property_desc = []
     property_price = []
     property_location = []
     property_beds = []
     property_baths = []
 
+    #initialize the mongo database connection
     my_client = MongoClient()
     db = my_client.scrapeCollection
     posts = db.posts
 
+    #running kafka locally
     my_producer3 = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
         value_serializer = lambda x:dumps(x).encode('utf-8')  
     )
 
-
+    #scrape the first page of the website only
     for page in range(1,2):
 
         url = 'https://www.property24.co.ke/1-bedroom-properties-to-rent-in-nairobi-p95?Bathrooms=1&PropertyTypes=houses%2capartments-flats%2ctownhouses&Page=' + str(page)
@@ -68,13 +70,17 @@ def main():
             }
 
             df = pd.DataFrame(data2)
-
+            
+            #json object, kafka can't recieve dataframes
             result = df.to_json(orient=None)
             
+            #scrapeTut10 is the kafka topic in use
             my_producer3.send('scrapeTut10', value=result)
 
+            #mongodb used as a data lake
             posts.insert_one(data2)
 
+            #timer to avoid getting blocked from the website
             time_wait = 5
             time.sleep(time_wait * 1)
 
